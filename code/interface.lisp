21ae0936dfd01d7ae79a3db4ce252cabbe08660a
Paul A. Patience <paul@apatience.com>
2024-06-04 17:11:34 -0400

    quaviver: add RESULT-TYPE parameter to *-TO-FLOAT
    * code/interface.lisp (bits-to-float, digits-to-float): Add RESULT-TYPE
    parameter after CLIENT.

--- code/interface.lisp
+++ code/interface.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:quaviver)
 
-(defgeneric bits-to-float (client bits))
+(defgeneric bits-to-float (client result-type bits))
 
 (defgeneric float-to-bits (client value))
 
-(defgeneric digits-to-float (client digits exponent))
+(defgeneric digits-to-float (client result-type digits exponent))
 
 (defgeneric float-to-digits (client value))
