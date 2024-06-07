5b0c67f143f867642e83c5b198cbe29cea35b2e8
Paul A. Patience <paul@apatience.com>
2024-06-07 09:27:03 -0400

    ieee754: unclutter packages.lisp file header
    * code/ieee754/packages.lisp: Remove BSD-3-Clause part from license and
    the later mention of Nibbles, since the file contains no code from
    Nibbles.

--- code/ieee754/packages.lisp
+++ code/ieee754/packages.lisp
@@ -1,12 +1,5 @@
 ;;;; SPDX-FileCopyrightText: Copyright (c) 2024 s-expressionists
-;;;; SPDX-License-Identifier: MIT AND BSD-3-Clause
-;;;;
-;;;; The implementations of QUAVIVER:BITS-FLOAT and QUAVIVER:FLOAT-BITS
-;;;; were ported, with modifications, from Nibbles [1], which is
-;;;; licensed under the BSD-3-Clause license.
-;;;; Any original code herein is licensed under the MIT license (Expat).
-;;;;
-;;;; [1]: https://github.com/sharplispers/nibbles
+;;;; SPDX-License-Identifier: MIT
 
 (cl:defpackage #:quaviver/ieee754
   (:use #:common-lisp)
