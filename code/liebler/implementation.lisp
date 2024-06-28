b61e2bb3941fd8d15bb78015d7bf0414d6243f40
Tarn W. Burton <twburton@gmail.com>
2024-06-28 12:58:33 -0400

    liebler: Add some type declarations

--- code/liebler/implementation.lisp
+++ code/liebler/implementation.lisp
@@ -13,6 +13,8 @@
                                  ,significand ,exponent ,sign)
          (let ((k (quaviver/math:floor-log-expt 2 10 ,exponent))
                (shift (- ,arithmetic-size (integer-length ,significand))))
+           (declare (type fixnum k shift ,exponent ,sign)
+                    (type quaviver/math::word ,significand))
            (setf ,significand (,round-to-odd (,expt10 (- ,exponent))
                                              (ash ,significand shift))
                  k (- k -1 shift)
