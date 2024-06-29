e2d2675ce455fb232cbb4117e18d073b2abc3c72
Tarn W. Burton <twburton@gmail.com>
2024-06-29 09:15:34 -0400

    liebler: improve floating point errors

--- code/liebler/implementation.lisp
+++ code/liebler/implementation.lisp
@@ -5,14 +5,33 @@
 
 (defmacro %liebler (client result-type significand exponent sign expt10 round-to-odd)
   (with-accessors ((arithmetic-size quaviver:arithmetic-size)
-                   (significand-size quaviver:significand-size))
+                   (significand-size quaviver:significand-size)
+                   (min-exponent quaviver:min-exponent)
+                   (max-exponent quaviver:max-exponent))
       result-type
     `(if (or (not (numberp ,exponent))
              (zerop ,significand))
          (quaviver:integer-float ,client ',result-type 2
                                  ,significand ,exponent ,sign)
-         (let ((k (quaviver/math:floor-log-expt 2 10 ,exponent))
-               (shift (- ,arithmetic-size (integer-length ,significand))))
+         (let* ((k (quaviver/math:floor-log-expt 2 10 ,exponent))
+                (q (+ k (- (integer-length ,exponent) ,significand-size)))
+                (shift (- ,arithmetic-size (integer-length ,significand))))
+           ;; The following overflow and underflow checks are not
+           ;; strict checks. Stricter checks will happen in
+           ;; integer-float/2. These are here to protect the expt10
+           ;; table lookup from an out of bounds error.
+           (when (> q ,(+ max-exponent
+                          (quaviver/math:ceiling-log-expt 2 10 1)))
+             (error 'floating-point-overflow
+                    :operation 'quaviver:integer-float
+                    :operands (list ,client ',result-type 10
+                                    ,significand ,exponent ,sign)))
+           (when (< q ,(- min-exponent
+                          (quaviver/math:ceiling-log-expt 2 10 1)))
+             (error 'floating-point-underflow
+                    :operation 'quaviver:integer-float
+                    :operands (list ,client ',result-type 10
+                                    ,significand ,exponent ,sign)))
            (setf ,significand (,round-to-odd (,expt10 (- ,exponent))
                                              (ash ,significand shift))
                  k (- k -1 shift)
