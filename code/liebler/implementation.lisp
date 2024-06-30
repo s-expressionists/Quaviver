658668f77d197035ec605acadb97d471f57d3bf3
Paul A. Patience <paul@apatience.com>
2024-06-30 16:37:25 -0400

    math: conform ROUND-TO-ODD to other operations
    Name and parameter order.

--- code/liebler/implementation.lisp
+++ code/liebler/implementation.lisp
@@ -35,8 +35,8 @@
                     :operation 'quaviver:integer-float
                     :operands (list ,client ',result-type 10
                                     ,significand ,exponent ,sign)))
-           (setf ,significand (,round-to-odd (,expt10 (- ,exponent))
-                                             (ash ,significand shift))
+           (setf ,significand (,round-to-odd (ash ,significand shift)
+                                             (,expt10 (- ,exponent)))
                  k (- k -1 shift)
                  shift (- ,significand-size (integer-length ,significand)))
            (quaviver:integer-float ,client ',result-type 2
@@ -49,14 +49,14 @@
   (%liebler client single-float
             significand exponent sign
             quaviver/math:expt10/32
-            quaviver/math:round-to-odd/32))
+            quaviver/math:round-to-odd/32-64))
 
 (defmethod quaviver:integer-float
     ((client client) (result-type (eql 'double-float)) (base (eql 10)) significand exponent sign)
   (%liebler client double-float
             significand exponent sign
             quaviver/math:expt10/64
-            quaviver/math:round-to-odd/64))
+            quaviver/math:round-to-odd/64-128))
 
 #+quaviver/long-float
 (defmethod quaviver:integer-float
@@ -64,4 +64,4 @@
   (%liebler client long-float
             significand exponent sign
             quaviver/math:expt10/128
-            quaviver/math:round-to-odd/128))
+            quaviver/math:round-to-odd/128-256))
