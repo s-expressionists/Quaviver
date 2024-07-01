504538b43a4d5252fe3b5fe3f703aa4415d027f2
Paul A. Patience <paul@apatience.com>
2024-07-01 13:49:00 -0400

    native: heed sign in INTEGER-FLOAT
    Previously, it would always return nonnegative values.
    
    * code/native/integer-float.lisp (quaviver:integer-float): Change the
    sign of the significand according to that provided. Also, introduce new
    specializations on 0 significand to handle signed zero.

--- code/native/integer-float.lisp
+++ code/native/integer-float.lisp
@@ -1,7 +1,21 @@
 (in-package #:quaviver/native)
 
 (defmethod quaviver:integer-float ((client client) result-type base significand exponent sign)
-  (coerce (if (minusp exponent)
-              (/ significand (expt base (- exponent)))
-              (* significand (expt base exponent)))
-          result-type))
+  (let ((significand (if (minusp sign) (- significand) significand)))
+    (coerce (if (minusp exponent)
+                (/ significand (expt base (- exponent)))
+                (* significand (expt base exponent)))
+            result-type)))
+
+(defmethod quaviver:integer-float
+    ((client client) (result-type (eql 'single-float)) base (significand (eql 0)) exponent sign)
+  (if (minusp sign) -0f0 0f0))
+
+(defmethod quaviver:integer-float
+    ((client client) (result-type (eql 'double-float)) base (significand (eql 0)) exponent sign)
+  (if (minusp sign) -0d0 0d0))
+
+#+quaviver/long-float
+(defmethod quaviver:integer-float
+    ((client client) (result-type (eql 'long-float)) base (significand (eql 0)) exponent sign)
+  (if (minusp sign) -0l0 0l0))
