b724564fdddc7c60eb26a962549389589ab03d39
Tarn W. Burton <twburton@gmail.com>
2024-07-07 08:42:13 -0400

    math: export support functions

--- code/math/packages.lisp
+++ code/math/packages.lisp
@@ -7,7 +7,15 @@
   (:export #:arithmetic-word
            #:hi/64
            #:hi/hi64/128
+           #:expt/32-10
+           #:expt/64-10
+           #:expt/128-10
+           #:expt/256-10
            #:expt
+           #:round-to-odd/32
+           #:round-to-odd/64
+           #:round-to-odd/128
+           #:round-to-odd/256
            #:round-to-odd
            #:floor-multiply/32-64q64
            #:floor-multiply/evenp/32-64q64
